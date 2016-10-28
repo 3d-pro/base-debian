bcb3a5fe08830dbde137c890d4e73cabc1d59089
Kittipun Khantitrirat <boot191@gmail.com>
2016-10-28 19:16:12 +0700

    Updated trigger script

--- trigger-openssh.sh
+++ trigger-openssh.sh
@@ -6,10 +6,4 @@ TRAVIS_BODY='{
   "branch":"master"
 }}'
 
-curl -s -X POST \
-  -H "Content-Type: application/json" \
-  -H "Accept: application/json" \
-  -H "Travis-API-Version: 3" \
-  -H "Authorization: token $TRAVIS_TOKEN" \
-  -d "$TRAVIS_BODY" \
-  https://api.travis-ci.org/repo/3d-pro%2Fopenssh/requests
+curl -s -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "Authorization: token $TRAVIS_TOKEN" -d "$TRAVIS_BODY" https://api.travis-ci.org/repo/3d-pro%2Fopenssh/requests
